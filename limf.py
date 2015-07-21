14271e3f304e5bf14ff4ff555a7eb147c53510c4
Mikołaj 'Lich' Halber <lich@cock.li>
2015-07-21 09:46:20 +0200

    Why have I done this.

--- limf.py
+++ limf.py
@@ -52,6 +52,7 @@ def encrypt_files(selected_host, only_link, file_name):
     cmd = 'gpg --batch --symmetric --cipher-algo AES256 --passphrase-fd 0 ' \
           '--output - {}'.format(source_filename)
     encrypted_output = Popen(shlex.split(cmd), stdout=PIPE, stdin=PIPE, stderr=PIPE)
+    encypted_data = encrypted_output.communicate(passphrase.encode())[0]
     return upload_files(encrypted_data, selected_host, only_link, file_name)+'!'+passphrase
 
 def decrypt_files(file_link):
