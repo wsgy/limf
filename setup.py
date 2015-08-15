8529be9846daf79091d75f03f807a0adcb4ab4af
Mikołaj 'Lich' Halber <lich@cock.li>
2015-08-15 15:23:23 +0200

    just removed any possibility of errors, will make it more advanced when I don't get some people to nag me that something is bad

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
 #!/bin/env python
 from setuptools import setup
-import os
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='limf',
-    version='0.3.1',
+    version='0.3.3',
     url='http://github.com/lich/limf',
     license='MIT',
     author='Mikołaj Halber',
     author_email='lich@openmailbox.com',
     description='A tool for uploading files to pomf.se clones',
-    long_description=read('README.md'),
+    long_description='Just read README.md. I will create .rst file when I want to do it.',
     packages=['limf'],
     install_requires=[
         'Requests>=2.6.0',
