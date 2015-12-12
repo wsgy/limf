51c4099724f5153e9f1fb9e89b597e4231cb83cd
wsgy <shpooman@gmail.com>
2015-12-12 22:19:36 +0900

    Update setup.py
    the ł is not recognized as Unicode on some systems, edited it so it works on most :P

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     version='0.4.1',
     url='http://github.com/lich/limf',
     license='MIT',
-    author='Mikołaj Halber',
+    author='Mikolaj Halber',
     author_email='lich@openmailbox.com',
     description='A tool for uploading files to pomf.se clones',
     long_description='Just read README.md. I will create .rst file when I want to do it.',
